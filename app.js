4a5de5eb2a7e5132edf09b8101ef58e2c8f24fb2
Rainier Ríos <96790657+Nedbor@users.noreply.github.com>
2022-12-22 15:57:12 -0500

    reaciendo el countdownApp

--- app.js
+++ app.js
@@ -1,31 +1,3 @@
-// Countdowm 
-const { body } = document;
-const days = document.getElementById('days');
-const hours = document.getElementById('hours');
-const minutes = document.getElementById('minutes');
-const seconds = document.getElementById('seconds');
-const currentYear = new Date().getFullYear();
-
-const xmastime = new Date(`December 25 ${currentYear} 00:00:00  GMT-5`);
-
-// Update countdown time
-function updateCountdown(){
-const currentTime = new Date();
-const diff = xmastime - currentTime;
-
-const d = Math.floor(diff / 1000 / 60 / 60 / 24);
-const h = Math.floor(diff / 1000 / 60 / 60) % 24;
-const m = Math.floor(diff / 1000 / 60) % 60;
-const s = Math.floor(diff / 1000) % 60;
-
-  days.innerHTML = days < 10 ? `0${days}` : days;
-  hours.innerHTML = hours < 10 ? `0${hours}` : hours;
-  minutes.innerHTML = minutes < 10 ? `0${minutes}` : minutes;
-  seconds.innerHTML = seconds < 10 ? `0${seconds}` : seconds;
-}
-
-setInterval(calculateChristmasCountdown, 1000); 
-
 
 
 // Some Snow Falling by Codepen.io
